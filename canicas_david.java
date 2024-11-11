53f01529f7dbd932db64b0b68d9894c50a1a2754
redondo519 <david.redalo@iesjulianmarias.es>
2024-11-11 21:17:12 +0100

    comit

--- canicas_david.java
+++ canicas_david.java
@@ -2,5 +2,6 @@ public class canicas_david {
     public static void main(String[] args) {
         //Canicas David
         System.out.println("CANICAS david");
+        //Operador ternario para hacer apuesta inferiror
     }
 }
