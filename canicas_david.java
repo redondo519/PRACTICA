0ab07c3bc3a1f0b20c923b5907ab43f499be583f
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2024-11-23 22:08:51 +0100

    comit

--- canicas_david.java
+++ canicas_david.java
@@ -63,8 +63,8 @@ public class canicas_david {
         int numCanicasJ2=10;    //se almacena las canicas del Jugador 2,  inicializada a 10.
         int apuestaJ1=-1;        //se almacena la apuesta del jugador 1.
         int apuestaJ2=-1;        //se almacena la apuesta del jugador 2.
-        String parImparJ1;      //se almacena la entrada del jugador 1 a la pregunta par/impar.
-        String parImparJ2;      //se almacena la entrada del jugador 2 a la pregunta par/impar.
+        String parImparJ1="par";      //se almacena la entrada del jugador 1 a la pregunta par/impar.
+        String parImparJ2="impar";      //se almacena la entrada del jugador 2 a la pregunta par/impar.
         boolean esPar=false;    //almacena true para par || false para impar.
         int turno; //almacena el turno (generado atleatoriamente) 0 para jugador 1, 1 para jugador 2
         int maxCanicas;
@@ -77,39 +77,77 @@ public class canicas_david {
         //condicion, inicio del juego en funcion de 0 o 1:  0-Inicia J1 \ 1-inicia J2
         if (turno == 0) {
 
-            System.out.println("************* TURNO JUGADOR 1 *************");
+            while(!(numCanicasJ1<=0)||(numCanicasJ2<=0)) {
 
-            // Determinar el máximo de canicas permitido
-            maxCanicas = Math.min(numCanicasJ1, numCanicasJ2);
 
-            //Pedimos apuesta jugador 1
-            while((apuestaJ1<=0) || (apuestaJ1>(maxCanicas))){
-                System.out.println("¿Cuantas canicas quieres apostar?: ");
-                apuestaJ1 = sc.nextInt();//LANZAR EXCEPCION
+
+                System.out.println("************* TURNO JUGADOR 1 *************");
+
+                // Determinar el máximo de canicas permitido
+                maxCanicas = Math.min(numCanicasJ1, numCanicasJ2);
+
+                //Pedimos apuesta jugador 1
+                do{
+                    System.out.println("¿Cuantas canicas quieres apostar?: ");
+                    apuestaJ1 = sc.nextInt();//LANZAR EXCEPCION
+                }while((apuestaJ1<=0) || (apuestaJ1>(maxCanicas)));
+
+                do {
+                    // Pedimos jugada par/impar Jugador 1
+                    System.out.println("¿Qué crees que tiene el Jugador 2 (par/impar)?: ");
+                    parImparJ1 = sc.next().toLowerCase(); // Convertir a minúsculas para facilitar la comparación
+                } while (!(parImparJ1.equals("par") || parImparJ1.equals("impar")));//LANZAR EXCEPCION
+
+
+
+                //generamos apuesta Jugador 2
+                apuestaJ2 = random.nextInt(1,maxCanicas+1 ); //Genera apuesta Jugador 2 entre 1 y maxcanicas incluidos
+
+
+                //Comprobar si apuesta J2 es par
+                if (apuestaJ2 % 2 == 0){
+                    esPar=true;
+                }
+
+                //Mostramos resultado
+                System.out.println("Resultado ");
+                //Condicion si entrada par/impar es correcta respecto a apuestaJ2
+                if (esPar){
+                    if (parImparJ1.equals("par")){
+                        numCanicasJ2 = numCanicasJ2 - apuestaJ2;
+                        numCanicasJ1 = numCanicasJ1 + apuestaJ1;
+                        System.out.printf("Jugador 1 GANAS %d canicas, ya que el Jugador 2 tenia %d canicas\n",apuestaJ1,apuestaJ2);
+                    }else if (parImparJ1.equals("impar")){
+                        numCanicasJ2 = numCanicasJ2 + apuestaJ2;
+                        numCanicasJ1 = numCanicasJ1 - apuestaJ1;
+                        System.out.printf("Jugador 1 PIERDES %d canicas, ya que el Jugador 2 tenia %d canicas\n",apuestaJ1,apuestaJ2);
+                    }
+                }else{
+                    if (parImparJ1.equals("impar")){
+                        numCanicasJ2 = numCanicasJ2 - apuestaJ2;
+                        numCanicasJ1 = numCanicasJ1 + apuestaJ1;
+                        System.out.printf("Jugador 1 GANAS %d canicas, ya que el Jugador 2 tenia %d canicas\n",apuestaJ1,apuestaJ2);
+                    }else if (parImparJ1.equals("par")){
+                        numCanicasJ2 = numCanicasJ2 + apuestaJ2;
+                        numCanicasJ1 = numCanicasJ1 - apuestaJ1;
+                        System.out.printf("Jugador 1 PIERDES %d canicas, ya que el Jugador 2 tenia %d canicas\n",apuestaJ1,apuestaJ2);
+                    }
+                }
+
+                System.out.printf("Jugador 1 tienes %d canicas\n",numCanicasJ1);
+                System.out.printf("Jugador 2 tienes %d canicas\n",numCanicasJ2);
+
+                System.out.println("*********** FIN TURNO JUGADOR 1 ***********");
             }
-
-            //Pedimos jugada par/impar Jugador 1
-            System.out.println("¿Que crees que tiene el Jugador 2 (par/impar)?: ");
-            parImparJ1 = sc.next(); //LANZAR EXCEPCION
-
-
-            //generamos apuesta Jugador 2
-            apuestaJ2 = random.nextInt(1,maxCanicas ); //
-
-            //Mostramos:
-            System.out.println("Canicas J1: "+numCanicasJ1);
-            System.out.println("Canicas J2: "+numCanicasJ2);
-            System.out.println("Canicas J1: "+apuestaJ1);
-            System.out.println("Apuesta J2: "+apuestaJ2);
-            System.out.println("par/impar J1: "+parImparJ1);
-
-
-
-
 
 
         } else {
             System.out.println("************* TURNO JUGADOR 2 *************");
+
+
+
+
+            System.out.println("*********** FIN TURNO JUGADOR 2 ***********");
         }
 
 
